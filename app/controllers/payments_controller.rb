9f643e1804267a3617cffec12cad7ee4c54791a3
Kapil <kchoudhary006@gmail.com>
2014-11-17 01:05:22 +0530

    basic payment feature done

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,36 +7,50 @@ class PaymentsController < ApplicationController
   end
 
   def access_setup
-    binding.pry
-    unless current_user.profile.stripe_access_cred
-      access_cred = current_user.profile.doctor_profile.build_stripe_access_cred
-      access_cred.access_token = params['code']
-      if access_cred.save
-        flash[:notice] = "You have completed your account setting!"
-      else
-        flash[:notice] = "You have completed your account setting!"
-      end
-      redirect_to profile_path(current_user.profile)
-    end
+	  access_cred = StripeAccessCred.new(doctor_profile_id: current_user.profile.doctor_profile)
+	  access_cred.access_token = params['code']
+	  if access_cred.save
+			flash[:notice] = "You have completed your account setting!"
+	  else
+			flash[:notice] = "You have completed your account setting!"
+	  end
+	  redirect_to profile_path(current_user.profile)
   end
 
   def transaction_fees
-    # Get the credit card details submitted by the form
-    token = params[:payment][:stripe_card_token]
+		# Get the credit card details submitted by the form
+		token = params[:payment][:stripe_card_token]
 
-    # Need to write code to fetch @payment_code code from db
-    response = HTTParty.post("https://connect.stripe.com/oauth/token?client_secret=#{ENV['CLIENT_SECRET']}&code=#{@payment_code}&grant_type=authorization_code")
-    
-    # Need to write code to fetch ACCESS_TOKEN from above response
-    # Create the charge on Stripe's servers - this will charge the user's card
-    charge = Stripe::Charge.create({
-        :amount => 1000, # amount incents
-        :currency => "usd",
-        :card => token,
-        :description => "payinguser@example.com",
-        :application_fee => 123 # amount incents
-      },
-      ACCESS_TOKEN #ACCESS_TOKEN # user's access token from the Stripe Connect flow
-    )
+		# This fetches payment code
+		payment_info = StripeAccessCred.all.order(:created_at => :asc).last
+
+		if payment_info
+			begin
+				response = HTTParty.post("https://connect.stripe.com/oauth/token?client_secret=#{ENV['CLIENT_SECRET']}&code=#{payment_info.access_token}&grant_type=authorization_code")
+				unless response['error']
+					# Create the charge on Stripe's servers - this will charge the user's card
+					charge = Stripe::Charge.create({
+							:amount => 1000,
+							:currency => "usd",
+							:card => token,
+							:description => "payinguser@example.com",
+							:application_fee => 23},
+							response['access_token']
+						)
+					if charge.paid
+						flash[:notice] = "Fee successfully submitted!"
+					else
+						flash[:error] = charge.failure_message
+					end
+				else
+					flash[:error] = response['error_description']
+				end
+			rescue Exception => e
+				# Need to manage a log file for this
+				flash[:error] = "Something went wrong!"
+			end
+		else
+			flash[:error] = "Doctor's stripe info is not setup!"
+		end
   end
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,4 @@
 Stripe.api_key = "sk_test_cd4HAw5OuNd60UjKawXBkX44"
 STRIPE_PUBLIC_KEY = "pk_test_nhbV3mU62ik2hMOHc75v25HX"
+ENV['CLIENT_ID'] = "ca_58jk2z8gQyQFKipXeCrMKzhDtbk8aPKr"
+ENV['CLIENT_SECRET'] = "sk_test_cd4HAw5OuNd60UjKawXBkX44"
