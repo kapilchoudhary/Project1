19abe33ccdfc4f0a3845f0ffcb2bdf542b09e894
Kapil <kchoudhary006@gmail.com>
2014-11-21 00:06:14 +0530

    admin creation script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(:email => "admin@eirmd.com", :password => 'admin!@#$', :password_confirmation => 'admin!@#$', :profile_type => 'admin' )
+
